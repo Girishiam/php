aa2439c40bdeb8bd920b59d91713d46b1b7b3ef5
Girish Mondal <girish.dcastalia@gmail.com>
2022-03-14 17:32:49 +0600

    constant and abstract classes

--- oops.php
+++ oops.php
@@ -90,6 +90,91 @@ $apple->set_name("Apple");
 echo "Fruit: " . $apple->get_name()." in " . $car->get_car();
 
 
+//inheritance//
+
+class FruitS {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color; 
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}."; 
+    echo "<br/>";
+  }
+}
+
+// Strawberry is inherited from Fruit
+class Strawberry extends FruitS {
+  public function message() {
+    echo "Am I a fruit or a berry? "; 
+    echo "<br/>";
+  }
+}
+
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();
+$strawberry->intro();
+
+
+
+
+
+//constant////////////////////////////////
+class hello {
+  const LEAVING_MESSAGE = "He there ! How are you?";
+  public function hihi() {
+    echo self::LEAVING_MESSAGE;
+    echo "<br/>";
+  }
+}
+
+$goodbye = new hello();
+$goodbye->hihi();
+
+
+//abstract classes////
+abstract class Car {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  abstract public function intro() : string;
+}
+
+
+class Audi extends Car {
+  public function intro() : string {
+    return "Choose German quality! I'm an $this->name!";
+  }
+}
+
+class Volvo extends Car {
+  public function intro() : string {
+    return "Proud to be Swedish! I'm a $this->name!";
+  }
+}
+
+class Citroen extends Car {
+  public function intro() : string {
+    return "French extravagance! I'm a $this->name!";
+  }
+}
+
+
+$audi = new audi("Audi");
+echo $audi->intro();
+echo "<br>";
+
+$volvo = new volvo("Volvo");
+echo $volvo->intro();
+echo "<br>";
+
+$citroen = new citroen("Citroen");
+echo $citroen->intro();
+
+
 
 
 ?>
